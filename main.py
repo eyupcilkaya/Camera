79ca89c95250a46cf82582e8f210eaa721ec325f
Uysal <uysalaltas@gmail.com>
2021-02-24 12:04:05 +0300

    Review changes

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from variables import Variables
 
 cam_data = Variables()
 
-camThread = threading.Thread(target=camera_control.Camera, args=(cam_data,))
-dbThread = threading.Thread(target=database_update.Update, args=(cam_data,))
-relayThread=threading.Thread(target=relay_control.Role, args=(cam_data,))
+camThread = threading.Thread(target=camera_control.camera, args=(cam_data,))
+dbThread = threading.Thread(target=database_update.update, args=(cam_data,))
+relayThread = threading.Thread(target=relay_control.role, args=(cam_data,))
+
 camThread.start()
 dbThread.start()
 relayThread.start()
--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 
-class Variables():
+class Variables:
 
     def __init__(self):
         print("...Initializing Camera Datas")
